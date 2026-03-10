b9133bb83287a9ac34bbc5654b9ca2c812ba9a19
krishavsoundarapandian25 <krishavsoundarapandian25>
2026-03-10 07:02:53 -0400

    I am Finished with My Java Script

--- javascript_1/script.js
+++
@@ -1,9 +0,0 @@
-//setup
-const canvas = document.getElementById("gameCanvas");
-const renderer = new THREE.WebGLRenderer({canvas});
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-const scene = new THREE.Scene ();
-scene.backhground = new THREE.Color(0x111111);
-
-const camera = new THREE
